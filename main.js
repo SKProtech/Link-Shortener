1a55b7ce40cc2880401caa3d105f4f65faf7ea6c
SKProtech <salauddinjamiu@gmail.com>
2021-06-17 21:18:01 +0100

    link

---
+++ main.js
@@ -0,0 +1,69 @@
+function $(el) {
+  return document.querySelector(el);
+}
+let link = $("#link"),
+  btn = $("#btn"),
+  loading = $(".loading"),
+  result = $(".col-result"),
+  copybtn = $("#copy"),
+  shorted = $("#shorted");
+
+btn.onclick = (function() {
+
+  if (link.value == "") {
+
+
+  } else {
+
+    shortURL(link.value);
+    
+    loading.style.display = "block";
+    
+    result.style.display = "none";
+
+  }
+
+})
+
+
+function shortURL(url) {
+  fetch("https://api.shrtco.de/v2/shorten?url=" + url, {
+    method: "POST"
+  }).then((response) => {
+    return response.json();
+
+  }).then((data) => {
+
+    result.style.display = "flex";
+    
+    shorted.innerText = data.result.full_short_link2;
+    
+    loading.style.display = "none";
+    
+    link.value = "";
+
+  }).catch((error) => {
+
+    loading.style.display = "none";
+    link.value = "";
+    console.log(error);
+
+  })
+
+}
+
+function copy(text) {
+  var input = document.createElement('textarea');
+  input.innerHTML = text;
+  document.body.appendChild(input);
+  input.select();
+  var resultCopy = document.execCommand("copy");
+  document.body.removeChild(input);
+  return resultCopy;
+}
+
+copybtn.onclick = () => {
+
+  copy(shorted.innerText);
+
+}
