fc68901400cccc2289537e7c864514c97982b68d
SKProtech <salauddinjamiu@gmail.com>
2021-06-18 01:41:46 +0100

    link

--- main.js
+++ main.js
@@ -6,19 +6,23 @@ let link = $("#link"),
   loading = $(".loading"),
   result = $(".col-result"),
   copybtn = $("#copy"),
+  toastBg = $(".toast-bg"),
+  toastmsg = $("#toast-msg"),
   shorted = $("#shorted");
 
 btn.onclick = (function() {
 
   if (link.value == "") {
 
+    toastShow("please enter url", "rgba(250,0,0,0.9)");
+
 
   } else {
 
     shortURL(link.value);
-    
+
     loading.style.display = "block";
-    
+
     result.style.display = "none";
 
   }
@@ -35,11 +39,11 @@ function shortURL(url) {
   }).then((data) => {
 
     result.style.display = "flex";
-    
+
     shorted.innerText = data.result.full_short_link2;
-    
+
     loading.style.display = "none";
-    
+
     link.value = "";
 
   }).catch((error) => {
@@ -62,8 +66,28 @@ function copy(text) {
   return resultCopy;
 }
 
+function toastShow(msg, color) {
+  var num = 0;
+  var time = setInterval(() => {
+
+    if (num == 3) {
+
+      clearInterval(time);
+      toastBg.style.display = "none";
+
+    } else {
+      toastBg.style.background = color;
+      toastBg.style.display = "flex";
+      toastmsg.innerText = msg;
+      num++;
+
+    }
+  }, 1000);
+}
+
 copybtn.onclick = () => {
 
   copy(shorted.innerText);
+  toastShow("Link Copied", "rgba(0,150,0,0.5)");
 
 }
